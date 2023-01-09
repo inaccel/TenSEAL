ea7581f31bc831f089a574500a000cc4fca3dd7c
Elias Koromilas <elias.koromilas@gmail.com>
2023-01-09 15:16:31 +0200

    Use InAccel SEAL repository

--- cmake/seal.cmake
+++ cmake/seal.cmake
@@ -2,11 +2,10 @@ include(FetchContent)
 
 set(SEAL_BUILD_DEPS ON)
 set(SEAL_USE_MSGSL ON)
-set(SEAL_USE_INTEL_HEXL OFF)
 
 FetchContent_Declare(
   com_microsoft_seal
-  GIT_REPOSITORY https://github.com/microsoft/SEAL
+  GIT_REPOSITORY https://github.com/inaccel/SEAL
   GIT_TAG        v4.0.0 #v4.0.0
 )
 FetchContent_MakeAvailable(com_microsoft_seal)
